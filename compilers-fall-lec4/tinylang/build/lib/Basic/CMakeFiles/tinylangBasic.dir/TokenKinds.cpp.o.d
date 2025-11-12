2e4d6d0f34751382e8a08434580adaaf1f1e6787
SkullSkif <likvlad662@gmail.com>
2025-11-12 19:12:24 +0700

    lab4 transfer

---
+++ compilers-fall-lec4/tinylang/build/lib/Basic/CMakeFiles/tinylangBasic.dir/TokenKinds.cpp.o.d
@@ -0,0 +1,10 @@
+lib/Basic/CMakeFiles/tinylangBasic.dir/TokenKinds.cpp.o: \
+ /home/vlad/Загрузки/compilers-fall-lec4(1)/compilers-fall-lec4/tinylang/lib/Basic/TokenKinds.cpp \
+ /usr/include/stdc-predef.h \
+ /home/vlad/Загрузки/compilers-fall-lec4(1)/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.h \
+ /usr/lib/llvm-21/include/llvm/Support/Compiler.h \
+ /usr/lib/llvm-21/include/llvm/Config/llvm-config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /home/vlad/Загрузки/compilers-fall-lec4(1)/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.def \
+ /usr/lib/llvm-21/include/llvm/Support/ErrorHandling.h \
+ /home/vlad/Загрузки/compilers-fall-lec4(1)/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.def
---
+++ oldlab4/compilers-fall-lec4/tinylang/build/lib/Basic/CMakeFiles/tinylangBasic.dir/TokenKinds.cpp.o.d
@@ -0,0 +1,10 @@
+lib/Basic/CMakeFiles/tinylangBasic.dir/TokenKinds.cpp.o: \
+ /home/vlad/KT2/rep/kt2/lab4/compilers-fall-lec4/tinylang/lib/Basic/TokenKinds.cpp \
+ /usr/include/stdc-predef.h \
+ /home/vlad/KT2/rep/kt2/lab4/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.h \
+ /usr/lib/llvm-18/include/llvm/Support/Compiler.h \
+ /usr/lib/llvm-18/include/llvm/Config/llvm-config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/13/include/stddef.h \
+ /home/vlad/KT2/rep/kt2/lab4/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.def \
+ /usr/lib/llvm-18/include/llvm/Support/ErrorHandling.h \
+ /home/vlad/KT2/rep/kt2/lab4/compilers-fall-lec4/tinylang/include/tinylang/Basic/TokenKinds.def
