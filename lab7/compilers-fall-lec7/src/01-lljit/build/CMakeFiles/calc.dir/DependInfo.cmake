9a8fc655b437d15f889050c3aca59fa9a4a95281
SkullSkif <likvlad662@gmail.com>
2025-12-04 07:38:17 +0700

    lab7init

---
+++ lab7/compilers-fall-lec7/src/01-lljit/build/CMakeFiles/calc.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/skif/KT2/rep/kt2/lab7/compilers-fall-lec7/src/01-lljit/Calc.cpp" "CMakeFiles/calc.dir/Calc.cpp.o" "gcc" "CMakeFiles/calc.dir/Calc.cpp.o.d"
+  "/home/skif/KT2/rep/kt2/lab7/compilers-fall-lec7/src/01-lljit/CodeGen.cpp" "CMakeFiles/calc.dir/CodeGen.cpp.o" "gcc" "CMakeFiles/calc.dir/CodeGen.cpp.o.d"
+  "/home/skif/KT2/rep/kt2/lab7/compilers-fall-lec7/src/01-lljit/Lexer.cpp" "CMakeFiles/calc.dir/Lexer.cpp.o" "gcc" "CMakeFiles/calc.dir/Lexer.cpp.o.d"
+  "/home/skif/KT2/rep/kt2/lab7/compilers-fall-lec7/src/01-lljit/Parser.cpp" "CMakeFiles/calc.dir/Parser.cpp.o" "gcc" "CMakeFiles/calc.dir/Parser.cpp.o.d"
+  "/home/skif/KT2/rep/kt2/lab7/compilers-fall-lec7/src/01-lljit/Sema.cpp" "CMakeFiles/calc.dir/Sema.cpp.o" "gcc" "CMakeFiles/calc.dir/Sema.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
